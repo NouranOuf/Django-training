cf295fe55a175cab76ffadbbe78d362519ad5e87
NouranOuf <nouranouf94@gmail.com>
2022-10-15 19:52:24 +0200

    Revert "git commit"
    This reverts commit 318cf68182a61cd37c282778c073e9982416bd19.

--- musicplatform/artists/views.py
+++ musicplatform/artists/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("artist page ")
+# Create your views here.
--- musicplatform/musicplatform/urls.py
+++ musicplatform/musicplatform/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('', include('artists.urls')),
     path('artists/', include('artists.urls')),
     path('albums/',include('albums.urls')),
     path('admin/', admin.site.urls),
